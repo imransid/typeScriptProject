a1f32209650e789ce4f89b9089f823f33dcb9b27
Imran Khan <emailofimran1992@gmail.com>
2020-10-24 17:25:09 +0600

    api call

--- src/@config/Sagas.js
+++ src/@config/Sagas.js
@@ -1,51 +1,52 @@
 import { takeEvery, select, call, put } from "redux-saga/effects";
 import actionType from '../@constants';
-// import { _all_Catagory } from "../saga_actions/Category";
 import { AuthService } from '../@services/apiService/authService';
 
-// const _AuthSignIn = (email) => {
-//     let mail=email.email
-//     let pass=email.password  
-// const payload={
-//     mail,
-//     pass
-// }
-
-// try {
-//           AuthService.login(payload).subscribe(
-//           (res) => {
-//             console.log("after response:", res);
-//             if (res.data.status === "success") {
-//               window.location.href = "/";
-//               console.log("logedin")
-//             }
-//           },
-//           (error) => {
-//             console.log(error);
-//           }
-//         );
-//       } catch (error) {
-//         console.log(error);
-//       }
-// }
-
-
-const apiCall = () => {
-  return true
-}
-
-
-// generator Func
-const _AuthSignIn = function* () {
+// api call
+export const _requestToApi = function* (action) {
   try {
 
-    const api_result = yield call(apiCall)
+    let mail = action.email;
+    let pass = action.password;
 
-    yield put({
-      type: actionType.AUTH_RESULT,
-      loading: false,
-      count: 1111111
-    })
+    const payload = {
+      mail,
+      pass
+    }
+
+    const result = yield AuthService.login(payload).subscribe(
+      (res) => {
+        console.log("after response:", res);
+
+        // return response 200 or error
+        if (res.data.status === "success") {
+          window.location.href = "/";
+          console.log("login")
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+
+
+    result.status === 201 ?
+      yield put({
+        type: actionType.AUTH_RESULT,
+        loading: false,
+        count: 1111111
+      }) : console.log('Login ERror password not match')
+  } catch (err) {
+    console.log('Error ', err)
+  }
+}
+
+// generator Func
+const _AuthSignIn = function* (action) {
+  try {
+
+    const api_result = yield call(_requestToApi, action);
 
   } catch (err) {
     console.log("ERROR Is", err)
